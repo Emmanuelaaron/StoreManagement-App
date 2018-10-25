04218cd8a05b0a626ca49b1baa8471ba00943c6e
Emmanuelaaron <emmanuelisabirye9@gmail.com>
2018-10-25 19:16:15 +0300

    An admin can get total number of sales and total price [Delivers #161252416]

--- api/models/sales.py
+++ api/models/sales.py
@@ -18,5 +18,11 @@ class Sales():
         self.sales_records.append(sales)
         self.number_of_sales =+ len(self.sales_records)
         price = int(sales["price"])
-        total_price =+ price
-        return (self.sales_records)
+        self.total_price += price
+        return self.sales_records
+
+    def get_sales(self):
+        if len(self.sales_records) == 0:
+            return "No sales yet"
+        else:
+            return ("No. of sales = "+  str(self.number_of_sales), "and the total price is "+ str(self.total_price))
